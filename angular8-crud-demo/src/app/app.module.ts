90ac21aae7ee8327a348dd3c980709fcc3326784
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-04 20:41:15 +0700

    #17 - Initial Project Angular 8 CRUD - Employee Service

---
+++ angular8-crud-demo/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CreateEmployeeComponent} from './employee/create-employee/create-employee.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {EmployeeService} from './service/employee.service';
+import {HttpClientModule} from '@angular/common/http';
+import {EmployeeListComponent} from './employee/employee-list/employee-list.component';
+import {UpdateEmployeeComponent} from './employee/update-employee/update-employee.component';
+import {DataTablesModule} from 'angular-datatables';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeListComponent,
+    UpdateEmployeeComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    DataTablesModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ angular8-crud-demo/src/app/model/employee.model.ts
@@ -0,0 +1,9 @@
+export class Employee {
+  id: number;
+  name: string;
+  email: string;
+  phone: number;
+  department: string;
+
+
+}
